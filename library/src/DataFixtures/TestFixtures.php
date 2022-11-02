ae6973d9e1bca6d89fa3956f8b44d182d1046e66
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-11-02 16:28:19 +0100

    add row data for user

--- library/src/DataFixtures/TestFixtures.php
+++ library/src/DataFixtures/TestFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
+use App\Entity\User;
 
 class TestFixtures extends Fixture
 {
@@ -11,7 +14,56 @@ class TestFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-
+        $this->loadUser($manager);
         $manager->flush();
+    }
+
+    public function loadUser(ObjectManager $manager) :void{
+        $userDatas = [
+            [
+                "email" => "admin@example.com",
+                "roles" => "ROLE_ADMIN",
+                "password" => "2y/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K",
+                "enabled" => true,
+                "created_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-01-01 09:00:00'),
+                "updated_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-01-01 09:00:00'),
+            ],
+            [
+                "email" => "foo.foo@example.com",
+                "roles" => "ROLE_EMRUNTEUR",
+                "password" => "2y/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K",
+                "enabled" => true,
+                "created_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-01-01 10:00:00'),
+                "updated_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-01-01 10:00:00'),
+            ],
+            [
+                "email" => "bar.bar@example.com",
+                "roles" => "ROLE_EMRUNTEUR",
+                "password" => "2y/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K",
+                "enabled" => false,
+                "created_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-02-01 11:00:00'),
+                "updated_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-05-01 12:00:00'),
+            ],
+            [
+                "email" => "baz.baz@example.com",
+                "roles" => "ROLE_EMRUNTEUR",
+                "password" => "2y/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K",
+                "enabled" => true,
+                "created_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-03-01 12:00:00'),
+                "updated_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-03-01 12:00:00'),
+            ]
+        ];
+
+        foreach ($userDatas as $userData){
+            $user = new User();
+            $user->setEmail($userData['email']);
+            $user->setRoles($userData['roles']);
+            $user->setPassword($userData['password']);
+            $user->setEnabled($userData['enabled']);
+            $user->setCreatedAt($userData['created_at']);
+            $user->setUpdatedAt($userData['updated_at']);
+
+            $manager->persist($user);
+        }
     }
 }
