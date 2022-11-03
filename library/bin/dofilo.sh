7b73a456b09b7c955931c558daba701edb2f0e0c
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-11-03 14:10:10 +0100

    revert change (problem with databse)

--- library/bin/dofilo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-php bin/console doctrine:database:drop --force --if-exists
-php bin/console doctrine:database:create --no-interaction
-php bin/console doctrine:migrations:migrate --no-interaction
-php bin/console doctrine:fixtures:load --no-interaction
